9367d91ec322e7fed5667810a91083145e29cdea
paul.murphy <paul.murphy@jet.com>
2019-03-22 16:13:15 +0000

    remove documentation.

--- cassandra-migration-spring-boot-starter/src/main/java/org/cognitor/cassandra/migration/spring/Keyspace.java
+++ cassandra-migration-spring-boot-starter/src/main/java/org/cognitor/cassandra/migration/spring/Keyspace.java
@@ -4,12 +4,7 @@ import org.springframework.data.annotation.Persistent;
 
 import java.lang.annotation.*;
 
-/**
- * Identifies a domain object to be persisted to Cassandra as a table.
- *
- * @author Alex Shvid
- * @author Matthew T. Adams
- */
+
 @Persistent
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
